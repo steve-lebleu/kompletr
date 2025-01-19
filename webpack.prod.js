1e63d8bf3091fb414ad4cfbc1d6dcd85c53e3b36
Steve Lebleu <steve@konfer.be>
2025-01-19 18:12:45 +0100

    chore: wait github release, package only dist content

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,7 +15,7 @@ export default [
       minimize: true
     },
     output: {
-      path: path.resolve(__dirname, 'dist/js'),
+      path: path.resolve(__dirname, './js'),
       filename: 'kompletr.min.js',
       library: {
         type: 'module',
