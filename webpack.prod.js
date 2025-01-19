c0a330655a4fa00d16201d9a21dc2b1a0aff0978
Steve Lebleu <steve@konfer.be>
2025-01-19 18:21:13 +0100

    chore: dist

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,7 +15,7 @@ export default [
       minimize: true
     },
     output: {
-      path: path.resolve(__dirname, './js'),
+      path: path.resolve(__dirname, 'js'),
       filename: 'kompletr.min.js',
       library: {
         type: 'module',
